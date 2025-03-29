9fdba625d6c5214cba540a543521ff05b0265b95
Nghia20032004 <luongtrongnghia816@gmail.com>
2025-03-29 08:30:47 +0700

    commit

---
+++ luongtrongnghia/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace luongtrongnghia.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
